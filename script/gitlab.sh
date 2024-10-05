de4fd6ce7d81df2445954bfe4f78ae8b288b24a6
Oussama Jamil <ojamil@e1r9p10.1337.ma>
2024-10-05 11:52:35 +0100

    test

--- script/gitlab.sh
+++ script/gitlab.sh
@@ -20,3 +20,31 @@ helm repo add gitlab https://charts.gitlab.io/
 helm search repo gitlab
 
 helm repo update
+
+
+
+echo -e "${CYAN}-----------gitlab done-------------------${RESET}"
+
+
+
+password=$(kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' -n gitlab | base64 --decode)
+echo -e "${CYAN}-----------gitlab password: ${password}  -------------------${RESET}"
+
+#wait for gitlab to be ready
+
+kubectl wait -n gitlab --for=condition=available deployment --all --timeout=-1s
+
+
+echo -e "${CYAN}-----------gitlab is ready-------------------${RESET}"
+
+#forwad port to access gitlab
+screen -dmS gitlab_port_forward bash -c 'kubectl port-forward --address 0.0.0.0 svc/gitlab-webservice-default -n gitlab 8085:8181'
+
+screen -dmS argocd_port_forward bash -c 'kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443'
+
+
+#  kubectl apply -f ${SOURCEDIR}/config/wil-playground/manifests/wil42.yaml
+ kubectl apply -f ${SOURCEDIR}/config/deployment.yaml
+
+echo -e "${CYAN}-----------gitlab port forward-------------------${RESET}"
+echo -e "${CYAN}-----------gitlab url http://gitlab.ojamil.com:8085-------------------${RESET}"
